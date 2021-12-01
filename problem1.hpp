4ce12e5b4a56ed234ec21e800c680bbf13783378
Tim Kluser <tkluser@student.ethz.ch>
2021-12-01 08:20:06 +0100

    day1

--- problem1.hpp
+++ problem1.hpp
@@ -6,20 +6,50 @@
 #include <sstream>
 using namespace std;
 
-void problem2(ifstream& file);
+void problem1(ifstream& file);
 
-void problem2(ifstream& file){
-    int pwdCount1 = 0; // for part 1
-    int pwdCount2 = 0; // for part 2
-    int nrLines = 0;
-    while(true){
-      //ifstream& line;
-      nrLines++;
-      string thisLine;
-      getline(file, thisLine); // get the line of the stream 
-      if (file.eof()) break;
+void problem1(ifstream& file){
+  // for task 1
+  int nrIncreases1 = 0;
+  int currNr;
+  int lastNr;
 
-      stringstream line(thisLine); // make a stringstream
-    }
+  // first two numbers
+  if (!file.eof()){
+    file >> lastNr;
+  }
+  if (!file.eof()){
+    file >> currNr;
+  }
+
+  while(true){
+    //ifstream& line;
+    if (lastNr < currNr) nrIncreases1++;
+    if (file.eof()) break;
+    lastNr = currNr;
+    file >> currNr;
+  }
+  cout << "Task1: " << nrIncreases1 << endl;
+  
+  // for task 2: the sum is bigger if the measurement + 3 is bigger
+  int nrIncreases2 = 0;
+  int measArr[4];
+  
+  ifstream file2("input.txt");
+  if (!file2.is_open()) cout << "couldn't open file " << endl;
+  for (int i = 0; i < 4; i++){
+    if (file2.eof()) break;
+    file2 >> measArr[i];
+  }
+  int firstElement = 0; // make calculations modulo
+  int lastElement = 3;
+  while(true){
+    if (measArr[(firstElement % 4)] < measArr[(lastElement % 4)]) nrIncreases2++;
+    if (file2.eof()) break;
+    file2 >> measArr[firstElement % 4];
+    firstElement++;
+    lastElement++;
+  }
+  cout << "Task2: " << nrIncreases2 << endl;
 }
 #endif
