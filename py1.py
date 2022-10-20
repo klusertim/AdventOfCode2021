32e4902f1ae57305a170557a0680120447130cf2
Tim Kluser <64404149+klusertim@users.noreply.github.com>
2022-10-20 11:21:46 +0200

    Update py1.py

--- py1.py
+++ py1.py
@@ -14,35 +14,29 @@ errors = []
 # iterate over each part of the image
 for x in range(w_-w):
     image_crop = image[:,x:x+w].flatten()
-
-
     compressed = np.dot(image_crop, eigenvectors)
-
     decompressed = mean + eigenvectors @ compressed
-
-
-
     #Compress and decompress the image patch and compute the reconstruction error.
-    # error is always bad
-    error = np.linalg.norm(decompressed - image_crop)
+    error = np.linalg.norm(decompressed - image_crop) 
+    
     errors.append(error)
 
-    # plot 2
+    # plot 1
     ax = []
     fig, ax = plt.subplots(2,1)
     rect = patches.Rectangle((x,0),w,h,linewidth=3,edgecolor='r',facecolor='none') 
 
     ax[0].imshow(image,cmap='gray')
-    ax[0].add_patch(rect) # donp't know what to write
+    ax[0].add_patch(rect)
     ax[1].plot(np.arange(x+1)+w//2,np.array(errors))
     ax[1].set_xlim([0,w_])
     IPython.display.clear_output(True)
     show()
-# unnessecary comment 
+    
+    
 x_best = np.argmin(errors)
 fig, ax = plt.subplots(2,1)
 rect = patches.Rectangle((x_best,0),w,h,linewidth=3,edgecolor='g',facecolor='none')
-
 ax[0].imshow(image,cmap='gray')
 ax[0].add_patch(rect)
 ax[1].plot(np.arange(x+1)+w//2,np.array(errors))
